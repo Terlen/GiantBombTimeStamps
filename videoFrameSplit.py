2b63ad7db28794aec3315cc20f26d551c785857d
Aidan Payne <aidan@apayne.net>
2021-05-02 09:43:55 -0700

    Added five digit padding to frame filenames

--- videoFrameSplit.py
+++ videoFrameSplit.py
@@ -2,7 +2,7 @@ from ffmpy import FFmpeg
 ff = FFmpeg(
     inputs = {"c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4":None},
     #global_options = {'y','r','1','s','640x360'},
-    outputs = {'thumbs/test-%d.jpg':'-vf fps=0.5'}
+    outputs = {'thumbs/test-%05d.jpg':'-vf fps=0.5'}
 )
 ff.cmd
 #'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -r 1 -s 640x360 -f image2 thumbs/test-%d.jpg'
