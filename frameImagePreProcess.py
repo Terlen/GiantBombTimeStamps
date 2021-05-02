b4dd372c440a435064c79111b605daa8c53f0e2c
Aidan Payne <aidan@apayne.net>
2021-05-02 09:41:11 -0700

    Deleted all files in thumbs directory, added loop for each frame to apply tophat

--- frameImagePreProcess.py
+++ frameImagePreProcess.py
@@ -1,15 +1,22 @@
 # Apply tophat morphological operation to highlight text
 import cv2
 import argparse
+import os
 
-image = cv2.imread("C:\\Users\\aidan\\Programming Projects\\GiantBombTimeStamps\\thumbs\\test-260.jpg")
+imagefiles = os.listdir(".\\thumbs\\")
+#print(imagefiles)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+for frame in imagefiles:
 
-rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13,5))
+    print(frame)
+    image = cv2.imread("C:\\Users\\aidan\\Programming Projects\\GiantBombTimeStamps\\thumbs\\"+frame)
 
-tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Original", image)
-cv2.imshow("Tophat", tophat)
-cv2.waitKey(0)
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13,5))
+
+    tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+
+    #cv2.imshow("Original", image)
+    cv2.imshow("Tophat", tophat)
+    cv2.waitKey(0)
--- videoFrameSplit.py
+++ videoFrameSplit.py
@@ -6,5 +6,5 @@ ff = FFmpeg(
 )
 ff.cmd
 #'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -r 1 -s 640x360 -f image2 thumbs/test-%d.jpg'
-'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -vf fps=0.5 thumbs/test-%d.jpg'
+'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -vf fps=0.5 thumbs/test-%05d.jpg'
 ff.run()
