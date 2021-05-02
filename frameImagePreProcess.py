c7e2e8e097a9ca1ed120adf02deb2d13cd03af6e
Aidan Payne <aidan@apayne.net>
2021-05-02 10:47:09 -0700

    Added code to extract text region from frame

--- frameImagePreProcess.py
+++ frameImagePreProcess.py
@@ -2,6 +2,8 @@
 import cv2
 import argparse
 import os
+import numpy as np
+import imutils
 
 imagefiles = os.listdir(".\\thumbs\\")
 #print(imagefiles)
@@ -13,10 +15,50 @@ for frame in imagefiles:
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13,5))
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,5))
+    sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 
     tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
 
-    #cv2.imshow("Original", image)
-    cv2.imshow("Tophat", tophat)
+    gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=1)
+    gradX = np.absolute(gradX)
+    (minVal, maxVal) = (np.min(gradX), np.max(gradX))
+    gradX = (255 * ((gradX - minVal) / (maxVal - minVal))).astype("uint8")
+
+    gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+    thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) [1]
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
+    thresh = cv2.erode(thresh, None, iterations=2)  
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    # loop over the contours
+    for c in cnts:
+            # compute the bounding box of the contour and use the contour to
+            # compute the aspect ratio and coverage ratio of the bounding box
+            # width to the width of the image
+            (x, y, w, h) = cv2.boundingRect(c)
+            ar = w / float(h)
+            crWidth = w / float(gray.shape[1])
+            # check to see if the aspect ratio and coverage width are within
+            # acceptable criteria
+            if ar > 3:
+                # pad the bounding box since we applied erosions and now need
+                # to re-grow it
+                pX = int((x + w) * 0.03)
+                pY = int((y + h) * 0.03)
+                (x, y) = (x - pX, y - pY)
+                (w, h) = (w + (pX * 2), h + (pY * 2))
+                # extract the ROI from the image and draw a bounding box
+                # surrounding the MRZ
+                roi = image[y:y + h, x:x + w].copy()
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                break
+
+    cv2.imshow("Original", image)
+    cv2.waitKey(0)
+    cv2.imshow("GradX", gradX)
+    cv2.waitKey(0)
+    cv2.imshow("Tophat", roi)
     cv2.waitKey(0)
