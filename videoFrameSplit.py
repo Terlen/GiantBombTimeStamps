d4a061be28dfc21a97d6c218b43c65db06b0db6b
Aidan Payne <aidan@apayne.net>
2021-05-14 21:06:18 -0700

    Updated frame sample rate to 1 frame per 5 seconds

--- videoFrameSplit.py
+++ videoFrameSplit.py
@@ -2,9 +2,9 @@ from ffmpy import FFmpeg
 ff = FFmpeg(
     inputs = {"c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4":None},
     #global_options = {'y','r','1','s','640x360'},
-    outputs = {'thumbs/test-%05d.jpg':'-vf fps=0.5'}
+    outputs = {'thumbs/test-%05d.jpg':'-vf fps=0.2'}
 )
 ff.cmd
 #'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -r 1 -s 640x360 -f image2 thumbs/test-%d.jpg'
-'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -vf fps=0.5 thumbs/test-%05d.jpg'
+'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -vf fps=0.2 thumbs/test-%05d.jpg'
 ff.run()
