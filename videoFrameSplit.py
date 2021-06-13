bee756cda42e8a13ec4c143366475d28ad71f46b
Aidan Payne <aidan@apayne.net>
2021-06-13 01:11:20 -0700

    Changed filename on extracted frames

--- videoFrameSplit.py
+++ videoFrameSplit.py
@@ -2,7 +2,7 @@ from ffmpy import FFmpeg
 ff = FFmpeg(
     inputs = {"c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4":None},
     #global_options = {'y','r','1','s','640x360'},
-    outputs = {'thumbs/test-%05d.jpg':'-vf fps=0.2'}
+    outputs = {'thumbs/%05d.jpg':'-vf fps=0.2'}
 )
 ff.cmd
 #'ffmpeg -i c:\\users\\aidan\\Videos\\Bombcast\\mc_bc_680_040621_9473_700.mp4 -r 1 -s 640x360 -f image2 thumbs/test-%d.jpg'
